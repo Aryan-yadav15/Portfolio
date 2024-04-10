e6489c64ff51d545adf878c12beefa463d577b57
Aryan Yadav <78091672+Aryan-yadav15@users.noreply.github.com>
2024-04-10 10:28:46 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+
+// Generate random positions for the blocks
+
+const bgAnimation = document.getElementById('bgAnimation');
+
+const numberOfColorBoxes = 600;
+
+for (let i = 0; i < numberOfColorBoxes; i++) {
+    const colorBox = document.createElement('div');
+    colorBox.classList.add('colorBox');
+    bgAnimation.append(colorBox)
+}
